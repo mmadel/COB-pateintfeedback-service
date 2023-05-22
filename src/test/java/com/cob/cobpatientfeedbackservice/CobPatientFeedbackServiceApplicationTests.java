dde6307cd24cfd2fc4f7bd012b918608edb4eef9
mmadel <mohammed.adel82@gmail.com>
2023-05-22 19:45:21 +0300

    init commit

---
+++ src/test/java/com/cob/cobpatientfeedbackservice/CobPatientFeedbackServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cob.cobpatientfeedbackservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CobPatientFeedbackServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
