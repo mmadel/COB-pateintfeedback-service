55343f85259c571ab7985b5c740d656fd3cf891e
mmadel <mohammed.adel82@gmail.com>
2023-06-08 18:53:43 +0300

    clean up the code

--- src/main/java/com/cob/feedback/repository/dashboard/hospitality/HospitalityRepository.java
+++
@@ -1,8 +0,0 @@
-package com.cob.feedback.repository.dashboard.hospitality;
-
-import com.cob.feedback.entity.FeedbackEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface HospitalityRepository extends JpaRepository<FeedbackEntity, Long> {
-
-}
