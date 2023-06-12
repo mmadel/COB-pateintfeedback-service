5e130a4dda5a4adb4c55c097cd9abd97e432ae91
mmadel <mohammed.adel82@gmail.com>
2023-06-12 18:04:48 +0300

    impl happy-index service

--- src/main/java/com/cob/feedback/CobPatientFeedbackServiceApplication.java
+++ src/main/java/com/cob/feedback/CobPatientFeedbackServiceApplication.java
@@ -1,13 +1,21 @@
 package com.cob.feedback;
 
+import com.cob.feedback.service.HospitalityService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class CobPatientFeedbackServiceApplication {
-
+public class CobPatientFeedbackServiceApplication  implements CommandLineRunner {
+	@Autowired
+	HospitalityService service;
 	public static void main(String[] args) {
 		SpringApplication.run(CobPatientFeedbackServiceApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+		service.retrieveHappyIndex(1685610000000L,1688158799000L,1);
+	}
 }
--- src/main/java/com/cob/feedback/service/HospitalityService.java
+++ src/main/java/com/cob/feedback/service/HospitalityService.java
@@ -1,12 +1,26 @@
 package com.cob.feedback.service;
 
+import com.cob.feedback.enums.FeedbackFeeling;
+import com.cob.feedback.formula.HappyIndexFormula;
+import com.cob.feedback.repository.HospitalityFeedbackRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
-public class HospitalityService  implements FeedbackService{
+public class HospitalityService implements FeedbackService {
+
+    @Autowired
+    HospitalityFeedbackRepository hospitalityFeedbackRepository;
+
     @Override
     public long retrieveHappyIndex(long dateFrom, long dateTo, long clinicId) {
-        return 0;
+        int[] feedbackValues =new int[4];
+        int counter =0;
+        for (FeedbackFeeling feeling : FeedbackFeeling.values()) {
+            feedbackValues[counter++] = hospitalityFeedbackRepository.count(dateFrom, dateTo, clinicId,feeling.label);
+        }
+        return HappyIndexFormula.calculate(100, feedbackValues[0],feedbackValues[1],feedbackValues[1],feedbackValues[3]);
+
     }
 
     @Override
