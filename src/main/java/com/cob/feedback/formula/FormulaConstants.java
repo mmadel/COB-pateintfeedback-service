8a3869029185e1d50bd80925c18c23d231a31436
mmadel <mohammed.adel82@gmail.com>
2023-06-12 15:41:54 +0300

    create formula foreach happy-index , nps and average

---
+++ src/main/java/com/cob/feedback/formula/FormulaConstants.java
@@ -0,0 +1,8 @@
+package com.cob.feedback.formula;
+
+public interface FormulaConstants {
+    int DEFAULT_STEP = 100;
+    String DEFAULT_POSITIVE_FRACTION = "66 2/3";
+    String DEFAULT_NEGATIVE_FRACTION = "33 1/3";
+    int[] FEEDBACK_WIGHT = {4, 3, 2, 1};
+}
