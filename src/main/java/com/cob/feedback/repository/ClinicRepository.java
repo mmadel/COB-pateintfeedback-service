557a7173d42d64f67a2b4aad7fac5e6da05cb7df
mmadel <mohammed.adel82@gmail.com>
2023-06-12 17:22:55 +0300

    impl repo foreach Hospitality and clinical to get count with date-range , clinic-id and feedback as an arguments

--- src/main/java/com/cob/feedback/repository/ClinicRepository.java
+++ src/main/java/com/cob/feedback/repository/ClinicRepository.java
@@ -1,7 +1,14 @@
 package com.cob.feedback.repository;
 
 import com.cob.feedback.entity.ClinicEntity;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.PagingAndSortingRepository;
 
 public interface ClinicRepository extends PagingAndSortingRepository<ClinicEntity, Long> {
+    @Query(value = "select count(id) from patient_feedback a " +
+            "where a.created_at>= :startDate " +
+            "and a.created_at<= :endDate " +
+            "and a.clinic_id= :clinicId " +
+            "and JSON_EXTRACT(feedback_questions, '$.clinicalFeedback')= :feedback", nativeQuery = true)
+    Integer count(Long startDate, Long endDate, Long clinicId, String feedback);
 }
--- src/main/java/com/cob/feedback/repository/HospitalityFeedbackRepository.java
+++ src/main/java/com/cob/feedback/repository/HospitalityFeedbackRepository.java
@@ -1,7 +1,14 @@
 package com.cob.feedback.repository;
 
 import com.cob.feedback.entity.FeedbackEntity;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.PagingAndSortingRepository;
 
 public interface HospitalityFeedbackRepository extends PagingAndSortingRepository<FeedbackEntity, Long> {
+    @Query(value = "select count(id) from patient_feedback a " +
+            "where a.created_at>= :startDate " +
+            "and a.created_at<= :endDate " +
+            "and a.clinic_id= :clinicId " +
+            "and JSON_EXTRACT(feedback_questions, '$.hospitalityFeedback')= :feedback", nativeQuery = true)
+    Integer count(Long startDate, Long endDate, Long clinicId, String feedback);
 }
