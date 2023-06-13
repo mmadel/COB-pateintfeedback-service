099efb75bbb047c724dfe21c14f2623ce557b96b
mmadel <mohammed.adel82@gmail.com>
2023-06-13 15:33:01 +0300

    create controller response advice response that will handle returning error from the controllers and ControllerErrorResponse that will hold the message response

---
+++ src/main/java/com/cob/feedback/excpetion/response/ControllerErrorResponseAdvisor.java
@@ -0,0 +1,25 @@
+package com.cob.feedback.excpetion.response;
+
+import com.cob.feedback.excpetion.business.FeedbackException;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.support.ResourceBundleMessageSource;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Component;
+
+import java.util.Locale;
+
+@Component
+@Slf4j
+public class ControllerErrorResponseAdvisor {
+    @Autowired
+    ResourceBundleMessageSource messageSource;
+
+    public ResponseEntity responseError(FeedbackException ex) {
+        String errorMessage = messageSource.getMessage(ex.getCode(), ex.getParameters(), Locale.ENGLISH);
+        ControllerErrorResponse controllerErrorResponse = new ControllerErrorResponse(errorMessage, ex.getStatus() == null ? HttpStatus.INTERNAL_SERVER_ERROR : ex.getStatus());
+        log.error(controllerErrorResponse.getMessage());
+        return new ResponseEntity(controllerErrorResponse, ex.getStatus() == null ? HttpStatus.INTERNAL_SERVER_ERROR : ex.getStatus());
+    }
+}
