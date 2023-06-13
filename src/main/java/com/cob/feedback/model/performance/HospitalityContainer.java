47e0a60b2f7eff0e7260a6a59dfad7b31606ddbe
mmadel <mohammed.adel82@gmail.com>
2023-06-13 13:52:13 +0300

    create Performance Index Container and its dependencies , Hospitality and clinical

---
+++ src/main/java/com/cob/feedback/model/performance/HospitalityContainer.java
@@ -0,0 +1,14 @@
+package com.cob.feedback.model.performance;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
+@Builder
+public class HospitalityContainer {
+    private long happyIndex;
+    private long nps;
+    private long average;
+}
