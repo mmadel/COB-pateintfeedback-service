15969e85fab2fe3c099409535e787dff0cc9effa
mmadel <mohammed.adel82@gmail.com>
2023-06-13 15:21:08 +0300

    add controller Advice

---
+++ src/main/java/com/cob/feedback/excpetion/ControllerExceptionHandler.java
@@ -0,0 +1,7 @@
+package com.cob.feedback.excpetion;
+
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+@ControllerAdvice
+public class ControllerExceptionHandler extends ResponseEntityExceptionHandler {
+}
