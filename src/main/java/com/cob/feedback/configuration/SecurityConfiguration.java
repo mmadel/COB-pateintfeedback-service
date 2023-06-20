fcee2c23332e248928e5e349e9d647dae3b50dcf
mmadel <mohammed.adel82@gmail.com>
2023-06-20 19:10:43 +0300

    add decoded bean

--- src/main/java/com/cob/feedback/configuration/SecurityConfiguration.java
+++ src/main/java/com/cob/feedback/configuration/SecurityConfiguration.java
@@ -20,7 +20,11 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.annotation.web.configurers.oauth2.server.resource.OAuth2ResourceServerConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.oauth2.jwt.JwtDecoder;
 import org.springframework.security.oauth2.jwt.JwtEncoder;
+import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
 import org.springframework.security.oauth2.jwt.NimbusJwtEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
@@ -63,9 +67,17 @@ public class SecurityConfiguration {
         return new RsaKeyProperties();
     }
     @Bean
+    JwtDecoder jwtDecoder() {
+        return NimbusJwtDecoder.withPublicKey(rsaKeys().publicKey).build();
+    }
+    @Bean
     JwtEncoder jwtEncoder() {
         JWK jwk = new RSAKey.Builder(rsaKeys().publicKey).privateKey(rsaKeys().privateKey).build();
         JWKSource<SecurityContext> jwks = new ImmutableJWKSet(new JWKSet(jwk));
         return new NimbusJwtEncoder(jwks);
+    }
+    @Bean
+    PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 }
