ca822e5ba92b73ff0dc44cf8948f9daeb367646f
mmadel <mohammed.adel82@gmail.com>
2023-06-21 03:14:38 +0300

    create clinic finder api

--- src/main/java/com/cob/feedback/repository/ClinicRepository.java
+++ src/main/java/com/cob/feedback/repository/ClinicRepository.java
@@ -3,12 +3,12 @@ package com.cob.feedback.repository;
 import com.cob.feedback.entity.ClinicEntity;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface ClinicRepository extends PagingAndSortingRepository<ClinicEntity, Long> {
-    @Query(value = "select count(id) from patient_feedback a " +
-            "where a.created_at>= :startDate " +
-            "and a.created_at<= :endDate " +
-            "and a.clinic_id= :clinicId " +
-            "and JSON_EXTRACT(feedback_questions, '$.clinicalFeedback')= :feedback", nativeQuery = true)
-    Integer count(Long startDate, Long endDate, Long clinicId, String feedback);
+
+    @Query("Select u.clinics from UserEntity u where u.id = :userId" )
+    List<ClinicEntity> findUserClinics(@Param("userId") Long userId);
 }
