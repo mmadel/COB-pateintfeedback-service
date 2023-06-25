4e09e7cb278dd6518450ee1708d2e47b27d33f78
mmadel <mohammed.adel82@gmail.com>
2023-06-26 02:56:39 +0300

    add api to return plain data that will be exported from excel report

--- src/main/java/com/cob/feedback/controller/FeedbackReportControllers.java
+++ src/main/java/com/cob/feedback/controller/FeedbackReportControllers.java
@@ -4,6 +4,8 @@ import com.cob.feedback.model.reports.ExcelReportCriteria;
 import com.cob.feedback.reports.excel.FeedbackExcelReportGenerator;
 import com.cob.feedback.service.reports.excel.ExcelReportService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletResponse;
@@ -22,5 +24,11 @@ public class FeedbackReportControllers {
         excelReportService.search(reportCriteria);
         FeedbackExcelReportGenerator feedbackExcelReportGenerator = new FeedbackExcelReportGenerator();
         feedbackExcelReportGenerator.export(response, excelReportService.getColumnsNames(),excelReportService.getData());
+    }
+
+    @PostMapping("/plain")
+    public ResponseEntity generatePlain(@RequestBody ExcelReportCriteria reportCriteria) throws IOException {
+        excelReportService.search(reportCriteria);
+        return new ResponseEntity(excelReportService.getData(), HttpStatus.OK);
     }
 }
